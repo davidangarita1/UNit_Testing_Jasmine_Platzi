0eb44e9afaba0a1a3a9515361f8a820845f82633
davidangarita1 <davidangarita.sistemas@gmail.com>
2021-06-03 16:35:35 -0500

    spect set and matcher added

---
+++ setPruebas.spec.js
@@ -0,0 +1,15 @@
+// Creando un set de pruebas
+describe("Componente", () => {
+  it("debería ...", () => {
+    expect(true).toBe(true);
+  });
+});
+
+// Agrupando por funcionalidad
+describe("Componente", () => {
+  describe("Funcionalidad uno", () => {
+    it("debería ...", () => {
+      expect(true).toBe(true);
+    });
+  });
+});
